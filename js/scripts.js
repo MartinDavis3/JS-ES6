6364e0f604144fcd069730619e1aae4cd2a46111
Martin Davis <martin_davis3@yahoo.ca>
2020-03-11 12:11:15 -0600

    add ES6 function definitions

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,152 @@ console.log( [1, 2, 3, 4].reduce( function ( a, v ) { return a + v; } ) );
 console.log( [1, 2, 3, 'a'].reduce( function ( a, v ) { return a + v; } ) );
 // The numbers are adding until it hits the string, then it concatenates.
 
+/**
+ * Let and Const
+ */
+
+var x=3; //Standard variable declaration. Function-scoped.
+let y=7;  // let is block-scoped.
+const z=36; // Const is block-scoped, but re-assignment is not allowed.
+
+for ( var myVar = 0; myVar < 10; myVar++ ) {
+    console.log( 'For loop iteration' )
+}
+console.log( 'myVar = ' + myVar);   // myVar is available as it is in the same function it was declared with var
+
+
+for ( let myNewVar = 0; myNewVar < 10; myNewVar++ ) {
+    console.log( 'For loop iteration' )
+}
+// console.log( 'myVar = ' + myNewVar);    // myNewVar is not available here because 'let' forced scope within the block (for loop)
+
+/**
+ * Const can't be re-assigned...but its insides can be manipulated
+ */
+
+const myObj = {
+    name: 'Henry',
+    age: 18
+}
+
+myObj.name = 'Sandy'; // Can we update properties or array elements inside of a const-declated variable. YES!!!
+console.log( myObj );
+
+// myObj = {};  This would give an error.
+
+/**
+ * Let's shake things up with: FUNCTIONS
+ */
+
+// this is the classic way of declaring the function
+function myFunction ( a, b ) {
+    return Number( a ) + Number( b );
+}
+
+// ES6 style:
+myFunction = ( a, b ) => Number( a ) + Number( b );
+
+console.log( myFunction( 3, 8 ) );
+
+//Bigger function (ES6)
+myOtherFunction = ( a, b ) => {     // use curly braces for larger, more complex operations.
+    const myAnswer = Number(a) + Number(b);
+    return myAnswer;
+}
+console.log( myOtherFunction( 34, 6 ) );
+
+/**
+ * Function default parameter values.
+ */
+
+ // We can set defaults to prevent errors in case nothing gets passed in fo a parameter
+addNums = ( x = 0, y = 0 ) => x + y;
+
+console.log( addNums() );
+
+/**
+ * Rest parameter.
+ */
+
+findHighNum = (  ...args ) => {    // ...args will capture any number of arguments that we pass!
+    console.log( args );           // It can be called by the name we had inbetween our parenthesis
+    return args;
+}
+
+addAllNums = ( ...nums ) => nums.reduce( ( a, v ) => a + v );  // nums becomes an array of length of the number of parameters passsed
+console.log( addAllNums( 64, 6, 10 ) );
+
+getSecondNum = ( ...nums ) => nums[1];
+console.log( getSecondNum( 64, 6, 10 ) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
