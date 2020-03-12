985b18fb0221b770ad9c98d0234557ab79df4361
Martin Davis <martin_davis3@yahoo.ca>
2020-03-12 14:08:57 -0600

    add static example

--- js/Calculator.Class.js
+++ js/Calculator.Class.js
@@ -1,5 +1,15 @@
-export class Calculator {
-    constructor () {
-        
+export class Calculator { 
+    static add( ...nums ) {
+        let sum = 0;
+        for ( let i = 0; i < nums.length; i++ ) {
+            sum += Number( nums[i] );
+        }
+        return sum;
     }
+    // With STATIC methods, we can call upon functionality without creating an
+    // object out of our class/blueprint! BEWARE we cannot use 'this'.
+    // There is no object to store our properties etc in...
+    static substract ( num1, num2 ) {
+        return Number( num1 ) - Number( num2 )
+    }       
 }
